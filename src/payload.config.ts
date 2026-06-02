911a85c7bc2703c9b1b4d767b7f39e60a09fa1a4
Niklas Höfling <hoefling.niklas@gmx.de>
2026-06-02 21:24:20 +0200

    chore: enable auto schema push in dev

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -43,6 +43,7 @@ export default buildConfig({
     pool: {
       connectionString: process.env.DATABASE_URL || '',
     },
+    push: process.env.NODE_ENV !== 'production',
   }),
   sharp,
   plugins: [],
